b0e11f00bed8d5346d993bbabf9fc31d375c634e
Asbjørn Enge <asbjorn@hanafjedle.net>
2013-06-08 08:13:25 +0200

    Replaced prompt with pure and added a few terminal dill

--- .zsh/prompt.zsh
+++ .zsh/prompt.zsh
@@ -1,32 +1,69 @@
-function git_prompt_info {
-  local ref=$(git symbolic-ref HEAD 2> /dev/null)
-  local gitst="$(git status 2> /dev/null)"
-  local pairname=${${${GIT_AUTHOR_EMAIL#pair+}%@github.com}//+/\/}
-  if [[ ${pairname} == 'ch' || ${pairname} == '' ]]; then
-    pairname=''
-  else
-    pairname=" ($pairname)"
-  fi
+# Pure
+# by Sindre Sorhus
+# https://github.com/sindresorhus/pure
+# MIT License
 
-  if [[ -f .git/MERGE_HEAD ]]; then
-    if [[ ${gitst} =~ "unmerged" ]]; then
-      gitstatus=" %{$fg[red]%}unmerged%{$reset_color%}"
-    else
-      gitstatus=" %{$fg[green]%}merged%{$reset_color%}"
-    fi
-  elif [[ ${gitst} =~ "Changes to be committed" ]]; then
-    gitstatus=" %{$fg[blue]%}!%{$reset_color%}"
-  elif [[ ${gitst} =~ "use \"git add" ]]; then
-    gitstatus=" %{$fg[red]%}!%{$reset_color%}"
-  elif [[ -n `git checkout HEAD 2> /dev/null | grep ahead` ]]; then
-    gitstatus=" %{$fg[yellow]%}*%{$reset_color%}"
-  else
-    gitstatus=''
-  fi
 
-  if [[ -n $ref ]]; then
-    echo "%{$fg_bold[green]%}/${ref#refs/heads/}%{$reset_color%}$gitstatus$pairname"
-  fi
+# Change this to your own username
+DEFAULT_USERNAME='asbjorn'
+
+# Threshold (sec) for showing cmd exec time
+CMD_MAX_EXEC_TIME=5
+
+
+# For my own and others sanity
+# git:
+# %b => current branch
+# %a => current action (rebase/merge)
+# prompt:
+# %F => color dict
+# %f => reset color
+# %~ => current path
+# %* => time
+# %n => username
+# %m => shortname host
+# %(?..) => prompt conditional - %(condition.true.false)
+
+autoload -Uz vcs_info
+zstyle ':vcs_info:*' enable git # You can add hg too if needed: `git hg`
+zstyle ':vcs_info:git*' formats ' %b'
+zstyle ':vcs_info:git*' actionformats ' %b|%a'
+
+# enable prompt substitution
+setopt PROMPT_SUBST
+
+# Only show username if not default
+[ $USER != $DEFAULT_USERNAME ] && local username='%n@%m '
+
+# Fastest possible way to check if repo is dirty
+git_dirty() {
+	# check if we're in a git repo
+	command git rev-parse --is-inside-work-tree &>/dev/null || return
+	# check if it's dirty
+	command git diff --quiet --ignore-submodules HEAD &>/dev/null; [ $? -eq 1 ] && echo '*'
 }
 
-PROMPT='%~%<< $(git_prompt_info)${PR_BOLD_WHITE}>%{${reset_color}%} '
+# Displays the exec time of the last command if set threshold was exceeded
+cmd_exec_time() {
+	local stop=`date +%s`
+	local start=${cmd_timestamp:-$stop}
+	let local elapsed=$stop-$start
+	[ $elapsed -gt $CMD_MAX_EXEC_TIME ] && echo ${elapsed}s
+}
+
+preexec() {
+	cmd_timestamp=`date +%s`
+}
+
+precmd() {
+	vcs_info
+	# Add `%*` to display the time
+	print -P '\n%F{blue}%~%F{236}$vcs_info_msg_0_`git_dirty` $username%f %F{yellow}`cmd_exec_time`%f'
+	# Reset value since `preexec` isn't always triggered
+	unset cmd_timestamp
+}
+
+# Prompt turns red if the previous command didn't exit with 0
+PROMPT='%(?.%F{magenta}.%F{red})❯%f '
+# Can be disabled:
+# PROMPT='%F{magenta}❯%f '
