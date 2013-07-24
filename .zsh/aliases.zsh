7fa89d0758b58424d269932e23f6a5806f133a42
Asbjørn Enge <asbjorn@hanafjedle.net>
2013-07-24 19:01:17 +0000

    Added --color for ls

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -1,5 +1,5 @@
 # Colorize output, add file type indicator, and put sizes in human readable format
-alias ls='ls -GFh'
+alias ls='ls -GFh --color'
 
 # Same as above, but in long listing format
 alias ll='ls -GFhl'
