7c6f88dec09aa35213e174c0475ba4b12ab56c17
Asbjørn Enge <asbjorn@hanafjedle.net>
2013-10-15 21:44:50 +0200

    Added two test sctipts

---
+++ i.sh
@@ -0,0 +1,9 @@
+git clone https://github.com/asbjornenge/dotfiles ~/.dotfiles
+ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/.dotfiles/.zsh ~/.zsh
+ln -s ~/.dotfiles/.zshrc ~/.zshrc
+ln -s ~/.dotfiles/.vimrc ~/.vimrc
+mkdir ~/.vim
+mkdir ~/.vim/backups
+mkdir ~/.vim/swaps
+mkdir ~/.vim/undo
