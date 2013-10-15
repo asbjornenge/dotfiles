35975c5684c165a9e6f3baed7fff4b533c824550
Asbjørn Enge <asbjorn@hanafjedle.net>
2013-10-15 11:15:52 +0200

    Added simple install script

---
+++ simple_install.sh
@@ -0,0 +1,8 @@
+ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/.dotfiles/.zsh ~/.zsh
+ln -s ~/.dotfiles/.zshrc ~/.zshrc
+ln -s ~/.dotfiles/.vimrc ~/.vimrc
+mkdir ~/.vim
+mkdir ~/.vim/backups
+mkdir ~/.vim/swaps
+mkdir ~/.vim/undo
